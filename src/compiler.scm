ab115de8f63838d6eecf055b3d77e9a2e8c7801b
Nada Amin <namin@alum.mit.edu>
2011-12-15 02:14:43 +0100

    1.3 Unary Primitives, ex1: compiler framework

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,4 +1,5 @@
 (load "tests-driver.scm")
+;;--- (load "tests-1.3-req.scm")
 (load "tests-1.2-req.scm")
 (load "tests-1.1-req.scm")
 
@@ -32,11 +33,53 @@
    [(char? x) (bitwise-ior (ash (char->integer x) charshift) chartag)]
    [else #f]))
 
-(define (emit-program x)
-  (unless (immediate? x) (error 'emit-program (format "'~s' not an immediate" x)))
+(define (emit-immediate x)
+  (emit "  movl $~s, %eax" (immediate-rep x)))
+
+(define-syntax define-primitive
+  (syntax-rules ()
+    [(_ (prim-name arg* ...) b b* ...)
+     (begin
+       (putprop 'prim-name '*is-prim* #t)
+       (putprop 'prim-name '*arg-count*
+         (length '(arg* ...)))
+       (putprop 'prim-name '*emitter*
+         (lambda (arg* ...) b b* ...)))]))
+
+(define (primitive? x)
+  (and (symbol? x) (getprop x '*is-prim*)))
+
+(define (primitive-emitter x)
+  (or (getprop x '*emitter*) (error 'primitive-emitter (format "primitive ~s has no emitter" x))))
+
+(define (primcall? expr)
+  (and (pair? expr) (primitive? (car expr))))
+
+(define (check-primcall-args prim args)
+  (= (getprop prim '*arg-count*) (length args)))
+
+(define (emit-primcall expr)
+  (let ([prim (car expr)] [args (cdr expr)])
+    (check-primcall-args prim args)
+    (apply (primitive-emitter prim) args)))
+
+(define-primitive ($fxadd1 arg)
+  (emit-expr arg)
+  (emit "  addl $~s, %eax" (immediate-rep 1)))
+
+(define (emit-expr expr)
+  (cond
+   [(immediate? expr) (emit-immediate expr)]
+   [(primcall? expr) (emit-primcall expr)]
+   [else (error 'emit-expr (format "~s is not an expression" expr))]))
+
+(define (emit-function-header f)
   (emit "  .text")
-  (emit "  .globl scheme_entry")
-  (emit "  .type scheme_entry, @function")
-  (emit "scheme_entry:")
-  (emit "  movl $~s, %eax" (immediate-rep x))
+  (emit "  .globl ~a" f)
+  (emit "  .type ~a, @function" f)
+  (emit "~a:" f))
+
+(define (emit-program x)
+  (emit-function-header "scheme_entry")
+  (emit-expr x)
   (emit "  ret"))
