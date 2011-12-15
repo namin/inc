78c342f4ef41ea1277a598f3756c0bfe0d947d7a
Nada Amin <namin@alum.mit.edu>
2011-12-15 01:02:45 +0100

    1.2 Immediate Constants
    Added booleans, empty list, chars.
    All 1.2 tests pass.

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,12 +1,16 @@
 (load "tests-driver.scm")
-;;(load "tests-1.2-req.scm")
+(load "tests-1.2-req.scm")
 (load "tests-1.1-req.scm")
 
-(define fxshift     2)
-(define fxmask   #x03)
-(define bool_f   #x2F)
-(define bool_t   #x6F)
-(define wordsize    4) ; bytes
+(define fxshift        2)
+(define fxmask      #x03)
+(define bool_f      #x2F)
+(define bool_t      #x6F)
+(define list_nil    #x3F)
+(define charshift      8)
+(define charmask    #x0F)
+(define chartag     #x0F)
+(define wordsize       4) ; bytes
 
 (define fixnum-bits (- (* wordsize 8) fxshift))
 
@@ -18,12 +22,15 @@
   (and (integer? x) (exact? x) (<= fxlower x fxupper)))
 
 (define (immediate? x)
-  (or (fixnum? x))) ;;(boolean? x) ---))
+  (or (fixnum? x) (boolean? x) (null? x) (char? x)))
 
 (define (immediate-rep x)
   (cond
    [(fixnum? x) (ash x fxshift)]
-   [else #f])) ;;---))
+   [(boolean? x) (if x bool_t bool_f)]
+   [(null? x) list_nil]
+   [(char? x) (bitwise-ior (ash (char->integer x) charshift) chartag)]
+   [else #f]))
 
 (define (emit-program x)
   (unless (immediate? x) (error 'emit-program (format "'~s' not an immediate" x)))
--- src/startup.c
+++ src/startup.c
@@ -3,9 +3,14 @@
 /* define all scheme constants */
 #define bool_f               0x2F
 #define bool_t               0x6F
+#define list_nil             0x3F
 #define fx_mask              0x03
 #define fx_tag               0x00
 #define fx_shift                2
+#define char_mask            0x0F
+#define char_tag             0x0F
+#define char_shift              8
+
 
 /* all scheme values are of type ptrs */
 typedef unsigned int ptr;
@@ -19,6 +24,15 @@ static void print_ptr(ptr x) {
     printf("#f");
   } else if (x == bool_t) {
     printf("#t");
+  } else if (x == list_nil) {
+    printf("()");
+  } else if ((x & char_mask) == char_tag) {
+    char c = (char) (x >> char_shift);
+    if      (c == '\t') printf("#\\tab");
+    else if (c == '\n') printf("#\\newline");
+    else if (c == '\r') printf("#\\return");
+    else if (c == ' ')  printf("#\\space");
+    else                printf("#\\%c", c);
   } else {
     printf("#<unknown 0x%08x>", x);
   }
