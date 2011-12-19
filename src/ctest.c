665825b5f2439c92713132f06cb7f0e671131512
Nada Amin <namin@alum.mit.edu>
2011-12-19 20:00:43 +0100

    new ctest, which shows how to call a dynamic label

--- src/ctest.c
+++ src/ctest.c
@@ -1,9 +1,20 @@
 #include "startup.h"
 
+char inc(char c) {
+  return c+1;
+}
+
+char do_call(char c, char (*fun)(char)) {
+  return fun(c);
+}
+
+char fancy(char c) {
+  return do_call(c, &inc);
+}
 ptr scheme_entry(context* ctxt, char* stack_base, char* heap){
   stack_base[-1] = 111;
   heap[0] = 'a';
-  heap[1] = heap[0];
+  heap[1] = fancy(heap[0]);
   ptr p = 0 | (heap[0] << char_shift) | char_tag;
   return p;
 }
