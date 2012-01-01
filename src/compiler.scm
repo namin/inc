382f3c70c928b5f8dc559c26a7bd3b30948189ea
Nada Amin <namin@alum.mit.edu>
2012-01-01 17:12:59 -0500

    Done with 4.2.

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,7 +1,5 @@
 (load "tests-driver.scm")
-(load "tests-4.2.3-req.scm")
-(load "tests-4.2.2-req.scm")
-(load "tests-4.2.1-req.scm")
+(load "tests-4.2-req.scm")
 (load "tests-4.1-req.scm")
 (load "tests-3.4-req.scm")
 (load "tests-3.3-req.scm")
