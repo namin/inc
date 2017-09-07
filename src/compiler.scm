5553a644f45496b799be2fa93ae94f29755653fa
Jaseem Abid <jaseemabid@gmail.com>
2017-09-07 19:08:55 +0530

    Step 2: Immediate constants; numbers and bool and ()
    Tests passing: 12

--- src/compiler.scm
+++ src/compiler.scm
@@ -2,8 +2,28 @@
 
 (load "tests-driver.scm")
 (load "tests-1.1-req.scm")
+(load "tests-1.2-req.scm")
 
 ;; Preamble
+
+(define bool-f     #b00101111)
+(define bool-t     #b01101111)
+(define boolshift           7)
+(define booltag    #b00011111)
+(define charmask   #b00111111)
+(define charshift           8)
+(define chartag    #b00001111)
+(define fxshift             2)
+(define fxtag               0)
+(define list-nil   #b00111111)
+
+(define (immediate-rep x)
+  (cond
+   [(integer? x) (ash x fxshift)]
+   [(boolean? x) (if x bool-t bool-f)]
+   [(null? x) list-nil]
+   [(char? x) (bitwise-ior (ash (char->integer x) charshift) chartag)]
+   [else #f]))
 
 (define (emit-label label)
   (emit "~a:" label))
@@ -16,5 +36,5 @@
 
 (define (compile-program x)
   (emit-function-header "scheme_entry")
-  (emit "    movl $~a, %eax" x)
+  (emit "    movl $~a, %eax" (immediate-rep x))
   (emit "    ret"))
--- src/startup.c
+++ src/startup.c
@@ -2,7 +2,34 @@
 
 extern int scheme_entry();
 
-int main() {
-    printf("%d\n", scheme_entry());
+#define bool_f     0b00101111
+#define bool_t     0b01101111
+#define boolshift           7
+#define booltag    0b00011111
+#define charmask   0b00111111
+#define fxmask     0b00000011
+#define charshift           8
+#define chartag    0b00001111
+#define fxshift             2
+#define fxtag               0
+#define list_nil   0b00111111
 
+int main() {
+    int val = scheme_entry();
+
+    if ((val & fxmask) == fxtag) {
+        printf("%d\n", val >> fxshift);
+
+    } else if (val == bool_t) {
+        printf("#t\n");
+
+    } else if (val == bool_f) {
+        printf("#f\n");
+
+    } else if (val == list_nil) {
+        printf("()\n");
+
+    } else {
+        printf("ERROR\n");
+    }
 }
