e4ebb73d341ede13591d64fce3e6c662c20f17e5
Jaseem Abid <jaseemabid@gmail.com>
2017-09-07 22:42:43 +0530

    Make integer checks a bit safer

--- src/compiler.scm
+++ src/compiler.scm
@@ -6,6 +6,7 @@
 
 ;; Preamble
 
+(define wordsize            8)
 (define bool-f     #b00101111)
 (define bool-t     #b01101111)
 (define boolshift           7)
@@ -17,9 +18,20 @@
 (define fxtag               0)
 (define list-nil   #b00111111)
 
+;; Range for fixnums
+(define fixnum-bits (- (* wordsize 8) fxshift))
+(define fxlower (- (expt 2 (- fixnum-bits 1))))
+(define fxupper (sub1 (expt 2 (- fixnum-bits 1))))
+
+(define (fixnum? x)
+  (and (integer? x) (exact? x) (<= fxlower x fxupper)))
+
+(define (immediate? x)
+  (or (fixnum? x) (boolean? x) (char? x) (null? x)))
+
 (define (immediate-rep x)
   (cond
-   [(integer? x) (ash x fxshift)]
+   [(fixnum? x) (ash x fxshift)]
    [(boolean? x) (if x bool-t bool-f)]
    [(null? x) list-nil]
    [(char? x) (bitwise-ior (ash (char->integer x) charshift) chartag)]
@@ -35,6 +47,7 @@
   (emit-label f))
 
 (define (compile-program x)
+  (unless (immediate? x) (error "" "Not an immediate value"))
   (emit-function-header "scheme_entry")
   (emit "    movl $~a, %eax" (immediate-rep x))
   (emit "    ret"))
