0924c3158d96dccdf060416b4c5e761b16ad8b0f
Jaseem Abid <jaseemabid@gmail.com>
2017-10-04 20:35:38 +0530

    Revert test order such that I see the relevant ones first

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,14 +1,15 @@
 ;; An incremental compiler
 
 (load "tests-driver.scm")
-(load "tests-1.1-req.scm")
-(load "tests-1.2-req.scm")
-(load "tests-1.3-req.scm")
-(load "tests-1.4-req.scm")
-(load "tests-1.5-req.scm")
-(load "tests-1.6-req.scm")
-(load "tests-1.7-req.scm")
+
 (load "tests-1.8-req.scm")
+(load "tests-1.7-req.scm")
+(load "tests-1.6-req.scm")
+(load "tests-1.5-req.scm")
+(load "tests-1.4-req.scm")
+(load "tests-1.3-req.scm")
+(load "tests-1.2-req.scm")
+(load "tests-1.1-req.scm")
 
 ;; Preamble
 
