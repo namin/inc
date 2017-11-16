f5394ab78b999030a983ec6022407975a867282e
Jaseem Abid <jaseemabid@gmail.com>
2017-11-16 19:41:36 +0530

    Move test files around
    Keep only used files in test and move everything else to unused. Add
    them one by one as the compiler matures. Its easier to refactor running
    tests alone.

--- src/compiler.scm
+++ src/compiler.scm
@@ -2,14 +2,14 @@
 
 (load "tests-driver.scm")
 
-(load "tests-1.8-req.scm")
-(load "tests-1.7-req.scm")
-(load "tests-1.6-req.scm")
-(load "tests-1.5-req.scm")
-(load "tests-1.4-req.scm")
-(load "tests-1.3-req.scm")
-(load "tests-1.2-req.scm")
-(load "tests-1.1-req.scm")
+(load "../tests/tests-1.8-req.scm")
+(load "../tests/tests-1.7-req.scm")
+(load "../tests/tests-1.6-req.scm")
+(load "../tests/tests-1.5-req.scm")
+(load "../tests/tests-1.4-req.scm")
+(load "../tests/tests-1.3-req.scm")
+(load "../tests/tests-1.2-req.scm")
+(load "../tests/tests-1.1-req.scm")
 
 ;; Constants
 (define wordsize             8)
