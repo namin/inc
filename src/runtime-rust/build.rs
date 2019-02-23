5e968f9235e423cf666e7709833b608384e5a8b2
Nada Amin <namin@alum.mit.edu>
2019-02-23 19:20:02 +0000

    use bindings

--- src/runtime-rust/build.rs
+++ src/runtime-rust/build.rs
@@ -1,7 +1,16 @@
 extern crate cc;
+extern crate bindgen;
 
 fn main() {
     cc::Build::new()
         .file("../stst.s")
         .compile("scheme-entry-lib");
+
+    let bindings = bindgen::Builder::default()
+        .header("bindings.h")
+        .generate()
+        .expect("Unable to generate bindings");
+    bindings
+        .write_to_file("src/bindings.rs")
+        .expect("Couldn't write bindings!");
 }
--- src/runtime-rust/src/main.rs
+++ src/runtime-rust/src/main.rs
@@ -1,8 +1,32 @@
-extern "C" {
-    fn scheme_entry() -> u32;
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+include!("bindings.rs");
+
+use std::os::raw::c_uint;
+use std::io::Write;
+
+enum PrintState {
+    OUT,
+    IN,
+    DISPLY
+}
+
+fn print_ptr_rec(mut port: std::io::Stdout, x: ptr, state: PrintState) {
+    if  (x & fx_mask) == fx_tag {
+        write!(port, "{}", (x as c_uint) >> fx_shift);
+    } else {
+        write!(port, "TODO");
+    }
+}
+fn print_ptr(x: ptr) {
+    print_ptr_rec(std::io::stdout(), x, PrintState::OUT);
+    println!("");
 }
 
 fn main() {
-   let r = unsafe { scheme_entry() };
-   println!("{}", r);
+    print_ptr(4);
 }
