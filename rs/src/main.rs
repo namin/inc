710df3787b90228507d08af8c5d932fe937c9470
Jaseem Abid <jaseemabid@monzo.com>
2019-03-24 02:54:49 +0000

    📝 Add the ability to generate code to a specified output file

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,33 +1,65 @@
-use std::io::{self, Read};
+use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
 
-// Expects the program in stdin, writes assembly to stdout
+// Usage: Expects the program in stdin, writes generated assembly to file name
+// specified as first argument or defaults to stdout.
+//
 fn main() -> std::io::Result<()> {
-    let mut program = String::new();
-    io::stdin()
-        .read_to_string(&mut program)
-        .expect("Expected a program in stdin");
+    let mut config = Config::new();
 
-    let i: i64 = program
+    let i: i64 = config
+        .program
         .trim_end()
         .parse()
         .expect("Failed to parse stdin to a valid program");
 
-    println!("{}\n", compile_program(i));
+    config
+        .outfile
+        .write_all(compile_program(i).as_bytes())
+        .expect("Failed to write generated code");
+
     Ok(())
 }
 
+struct Config {
+    program: String,
+    outfile: File,
+}
+
+impl Config {
+    fn new() -> Config {
+        let args: Vec<String> = env::args().collect();
+
+        let mut program = String::new();
+
+        io::stdin()
+            .read_to_string(&mut program)
+            .expect("Expected a program in stdin");
+
+        // impl Writer
+        let outfile = if args.len() == 1 {
+            File::open("/dev/stdout").unwrap()
+        } else {
+            File::create(args[1].clone()).expect("Failed to open output file")
+        };
+
+        Config { program, outfile }
+    }
+}
+
 fn emit_label(label: &str) -> String {
-    return format!("{}:\n", label);
+    format!("{}:\n", label)
 }
 
 #[cfg(target_os = "macos")]
 fn emit_function_header(name: &str) -> String {
     let mut ctx = String::new();
 
-    ctx.push_str(&format!("  .section __TEXT,__text\n"));
+    ctx.push_str("  .section __TEXT,__text\n");
     ctx.push_str(&format!("  .globl {}\n", &name));
     ctx.push_str(&emit_label(&name));
-    return ctx
+    ctx
 }
 
 fn compile_program(value: i64) -> String {
@@ -35,8 +67,8 @@ fn compile_program(value: i64) -> String {
 
     ctx.push_str(&emit_function_header("_init")[..]);
     ctx.push_str(&format!("  movq ${}, %rax\n", value));
-    ctx.push_str(&format!("  retq\n"));
-    return ctx;
+    ctx.push_str("  retq\n");
+    ctx
 }
 
 // ---
