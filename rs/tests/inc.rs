66fac77f811904c42b1cb95f0189f73dc3c9265a
Jaseem Abid <jaseemabid@monzo.com>
2019-06-08 14:27:06 +0100

    Clean up function tests a bit; several small improvements

--- rs/tests/inc.rs
+++ rs/tests/inc.rs
@@ -2,8 +2,7 @@
 extern crate inc;
 
 use inc::*;
-use std::fs;
-use std::process::Command;
+use std::{fs, process::Command};
 use uuid::Uuid;
 
 #[cfg(test)]
@@ -12,210 +11,208 @@ extern crate quickcheck;
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
 
-mod steps {
+// Step 1: Integers
+mod integers {
     use super::*;
 
-    // Step 1: Integers
     #[test]
-    fn integers() {
-        let tests = vec![
-            ("0", "0"),
-            ("1", "1"),
-            ("-1", "-1"),
-            ("10", "10"),
-            ("-10", "-10"),
-            ("2736", "2736"),
-            ("-2736", "-2736"),
-            ("536870911", "536870911"),
-            ("-536870912", "-536870912"),
+    fn unit() {
+        for t in &[0, 1, -1, 10, -10, 2736, -2736, 536870911, -536870912] {
+            test1(&t.to_string(), &t.to_string());
+        }
+    }
+
+    #[quickcheck]
+    fn quick(i: i64) -> () {
+        test1(&i.to_string(), &i.to_string())
+    }
+}
+
+// Step 2: Immediate constants
+mod immediate {
+    use super::*;
+
+    #[test]
+    fn unit() {
+        let tests = [
+            r"#f",
+            r"#t",
+            "()",
+            r"#\tab",
+            r"#\newline",
+            r"#\return",
+            r"#\space",
+            r"#\!",
+            r"#\#",
+            r"#\$",
+            r"#\%",
+            r"#\&",
+            r"#\'",
+            r"#\(",
+            r"#\)",
+            r"#\*",
+            r"#\+",
+            r"#\,",
+            r"#\-",
+            r"#\.",
+            r"#\/",
+            r"#\0",
+            r"#\9",
+            r"#\:",
+            r"#\;",
+            r"#\<",
+            r"#\=",
+            r"#\>",
+            r"#\?",
+            r"#\@",
+            r"#\A",
+            r"#\B",
+            r"#\Z",
+            r"#\(",
+            r"#\\",
+            r"#\]",
+            r"#\^",
+            r"#\_",
+            r"#\`",
+            r"#\a",
+            r"#\b",
+            r"#\z",
+            r"#\{",
+            r"#\|",
+            r"#\}",
+            r"#\~",
         ];
 
-        for (inp, out) in tests.iter() {
-            test1(String::from(*inp), String::from(*out));
-        }
-    }
-
-    // Step 2: Immediate constants
-    #[test]
-    fn constants() {
-        let tests = vec![
-            (r"#f", r"#f"),
-            (r"#t", r"#t"),
-            ("()", r"()"),
-            (r"#\tab", r"#\tab"),
-            (r"#\newline", r"#\newline"),
-            (r"#\return", r"#\return"),
-            (r"#\space", r"#\space"),
-            (r"#\!", r"#\!"),
-            (r"#\#", r"#\#"),
-            (r"#\$", r"#\$"),
-            (r"#\%", r"#\%"),
-            (r"#\&", r"#\&"),
-            (r"#\'", r"#\'"),
-            (r"#\(", r"#\("),
-            (r"#\)", r"#\)"),
-            (r"#\*", r"#\*"),
-            (r"#\+", r"#\+"),
-            (r"#\,", r"#\,"),
-            (r"#\-", r"#\-"),
-            (r"#\.", r"#\."),
-            (r"#\/", r"#\/"),
-            (r"#\0", r"#\0"),
-            (r"#\9", r"#\9"),
-            (r"#\:", r"#\:"),
-            (r"#\;", r"#\;"),
-            (r"#\<", r"#\<"),
-            (r"#\=", r"#\="),
-            (r"#\>", r"#\>"),
-            (r"#\?", r"#\?"),
-            (r"#\@", r"#\@"),
-            (r"#\A", r"#\A"),
-            (r"#\B", r"#\B"),
-            (r"#\Z", r"#\Z"),
-            (r"#\(", r"#\("),
-            (r"#\\", r"#\\"),
-            (r"#\]", r"#\]"),
-            (r"#\^", r"#\^"),
-            (r"#\_", r"#\_"),
-            (r"#\`", r"#\`"),
-            (r"#\a", r"#\a"),
-            (r"#\b", r"#\b"),
-            (r"#\z", r"#\z"),
-            (r"#\{", r"#\{"),
-            (r"#\|", r"#\|"),
-            (r"#\}", r"#\}"),
-            (r"#\~", r"#\~"),
-        ];
-
-        for (inp, out) in tests.iter() {
-            test1(String::from(*inp), String::from(*out));
-        }
-    }
-
-    // Step 3: Unary functions
-    mod unary {
-        use super::*;
-
-        #[test]
-        fn inc() {
-            let tests = vec![
-                (r"(inc 0)", r"1"),
-                (r"(inc -1)", r"0"),
-                (r"(inc 41)", r"42"),
-                (r"(inc -100) ", r"-99"),
-                (r"(inc 1000) ", r"1001"),
-                (r"(inc 536870910) ", r"536870911"),
-                (r"(inc -536870912) ", r"-536870911"),
-                (r"(inc (inc (inc (inc 1)))) ", r"5"),
-            ];
-
-            for (inp, out) in tests.iter() {
-                test1(String::from(*inp), String::from(*out));
-            }
-        }
-
-        #[test]
-        fn types() {
-            let tests = vec![
-                ("(fixnum? 0)", "#t"),
-                ("(fixnum? 1)", "#t"),
-                ("(fixnum? -1)", "#t"),
-                ("(fixnum? 37287)", "#t"),
-                ("(fixnum? -23873)", "#t"),
-                ("(fixnum? 536870911)", "#t"),
-                ("(fixnum? -536870912)", "#t"),
-                ("(fixnum? #t)", "#f"),
-                ("(fixnum? #f)", "#f"),
-                ("(fixnum? ())", "#f"),
-                ("(fixnum? #\\Q)", "#f"),
-                ("(char? #\\Q)", "#t"),
-                ("(char? 0)", "#f"),
-                ("(char? 1)", "#f"),
-                ("(char? -1)", "#f"),
-                ("(char? 37287)", "#f"),
-                ("(char? -23873)", "#f"),
-                ("(char? 536870911)", "#f"),
-                ("(char? -536870912)", "#f"),
-                ("(char? #t)", "#f"),
-                ("(char? #f)", "#f"),
-                ("(char? ())", "#f"),
-                ("(not 1)", "#f"),
-                ("(not #t)", "#f"),
-                ("(not #f)", "#t"),
-                ("(not ())", "#f"),
-            ];
-
-            for (inp, out) in tests.iter() {
-                test1(String::from(*inp), String::from(*out));
-            }
-        }
-
-        #[test]
-        fn zn() {
-            let tests = vec![
-                (r"(zero? 0)", r"#t"),
-                (r"(zero? 1)", r"#f"),
-                (r"(zero? #t)", r"#f"),
-                (r"(zero? ())", r"#f"),
-                (r"(null? ())", r"#t"),
-                (r"(null? #\Q)", r"#f"),
-                (r"(null? #f)", r"#f"),
-                (r"(null? #t)", r"#f"),
-                (r"(null? -23873)", r"#f"),
-                (r"(null? 37287)", r"#f"),
-                (r"(null? #\a)", r"#f"),
-            ];
-
-            for (inp, out) in tests.iter() {
-                test1(String::from(*inp), String::from(*out));
-            }
-        }
-    }
-
-    // Step 3: Unary functions
-    mod binary {
-        use super::*;
-
-        #[test]
-        fn plus() {
-            let tests = vec![(r"(+ 10 20)", r"30"), (r"(+ 40 (inc (inc 0)))", r"42")];
-
-            for (inp, out) in tests.iter() {
-                test1(String::from(*inp), String::from(*out));
-            }
+        for t in tests.iter() {
+            test1(t, t);
         }
     }
 }
 
-mod quick {
+// Step 3: Unary Primitives
+mod unary {
     use super::*;
 
-    #[quickcheck]
-    fn int64(i: i64) -> () {
-        test1(i.to_string(), i.to_string())
-    }
+    #[test]
+    fn inc() {
+        let tests = vec![
+            ("(inc 0)", "1"),
+            ("(inc -1)", "0"),
+            ("(inc 41)", "42"),
+            ("(inc -100) ", "-99"),
+            ("(inc (inc (inc (inc 1))))", "5"),
+        ];
 
-    #[quickcheck]
-    fn plus(x: i64, y: i64) -> () {
-        test1(format!("(+ {} {})", x, y), (x + y).to_string())
-    }
-
-    #[quickcheck]
-    fn multiply(x: i64, y: i64) -> () {
-        test1(format!("(* {} {})", x, y), (x * y).to_string())
-    }
-
-    #[quickcheck]
-    fn divide(x: i64, y: i64) -> () {
-        if y != 0 {
-            test1(format!("(/ {} {})", (x * y), y), x.to_string())
+        for (inp, out) in tests.iter() {
+            test1(inp, out);
         }
     }
 
-    #[quickcheck]
-    fn remainder(x: i64, y: i64) -> () {
-        if y != 0 {
-            test1(format!("(% {} {})", x, y), (x % y).to_string())
+    #[test]
+    fn types() {
+        let tests = [
+            ("(fixnum? 0)", "#t"),
+            ("(fixnum? 1)", "#t"),
+            ("(fixnum? -1)", "#t"),
+            ("(fixnum? 536870911)", "#t"),
+            ("(fixnum? -536870912)", "#t"),
+            ("(fixnum? #t)", "#f"),
+            ("(fixnum? #f)", "#f"),
+            ("(fixnum? ())", "#f"),
+            ("(fixnum? #\\Q)", "#f"),
+            ("(char? #\\Q)", "#t"),
+            ("(char? 0)", "#f"),
+            ("(char? 1)", "#f"),
+            ("(char? -1)", "#f"),
+            ("(char? 536870911)", "#f"),
+            ("(char? -536870912)", "#f"),
+            ("(char? #t)", "#f"),
+            ("(char? #f)", "#f"),
+            ("(char? ())", "#f"),
+            ("(not 1)", "#f"),
+            ("(not #t)", "#f"),
+            ("(not #f)", "#t"),
+            ("(not ())", "#f"),
+        ];
+
+        for (inp, out) in tests.iter() {
+            test1(inp, out);
+        }
+    }
+
+    #[test]
+    fn zero() {
+        let tests = vec![
+            (r"(zero? 0)", r"#t"),
+            (r"(zero? 1)", r"#f"),
+            (r"(zero? #t)", r"#f"),
+            (r"(zero? ())", r"#f"),
+        ];
+
+        for (inp, out) in tests.iter() {
+            test1(inp, out);
+        }
+    }
+
+    #[test]
+    fn null() {
+        let tests = vec![
+            (r"(null? ())", r"#t"),
+            (r"(null? #\Q)", r"#f"),
+            (r"(null? #f)", r"#f"),
+            (r"(null? #t)", r"#f"),
+            (r"(null? -23873)", r"#f"),
+            (r"(null? 37287)", r"#f"),
+            (r"(null? #\a)", r"#f"),
+        ];
+
+        for (inp, out) in tests.iter() {
+            test1(inp, out);
+        }
+    }
+
+}
+
+// Step 4: Binary primitives
+mod binary {
+    mod unit {
+        use super::super::*;
+
+        #[test]
+        fn plus() {
+            let tests = [("(+ 10 20)", "30"), ("(+ 40 (inc (inc 0)))", "42")];
+
+            for (inp, out) in tests.iter() {
+                test1(inp, out);
+            }
+        }
+    }
+
+    mod quick {
+        use super::super::*;
+
+        #[quickcheck]
+        fn plus(x: i64, y: i64) -> () {
+            test1(&format!("(+ {} {})", x, y), &(x + y).to_string())
+        }
+
+        #[quickcheck]
+        fn multiply(x: i64, y: i64) -> () {
+            test1(&format!("(* {} {})", x, y), &(x * y).to_string())
+        }
+
+        #[quickcheck]
+        fn divide(x: i64, y: i64) -> () {
+            if y != 0 {
+                test1(&format!("(/ {} {})", (x * y), y), &x.to_string())
+            }
+        }
+
+        #[quickcheck]
+        fn remainder(x: i64, y: i64) -> () {
+            if y != 0 {
+                test1(&format!("(% {} {})", x, y), &((x % y).to_string()))
+            }
         }
     }
 }
@@ -251,9 +248,9 @@ fn build(mut config: &mut Config) -> bool {
 }
 
 // Run a single test, assert everything and cleanup afterwards
-fn test1(input: String, output: String) {
+fn test1(input: &str, output: &str) {
     // Create a fresh config per run, this should allow for parallelism later.
-    let mut config = config(input);
+    let mut config = config(input.to_string());
 
     // Rebuild before every run
     assert!(build(&mut config));
@@ -268,13 +265,7 @@ fn test1(input: String, output: String) {
         format!("Failed to run binary `{}`", &config.output)
     );
 
-    assert_eq!(
-        String::from_utf8(proc.stdout)
-            .unwrap()
-            .trim()
-            .replace("\n", ""),
-        output
-    );
+    assert_eq!(String::from_utf8(proc.stdout).unwrap().trim(), output);
 
     // Clean up all the intermediary files generated
     fs::remove_file(&config.asm()).expect("Failed to clear generated asm files");
