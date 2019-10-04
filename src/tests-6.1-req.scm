1ff7c28320ccfa325beb81da728bf456ddbee0ee
Nada Amin <namin@alum.mit.edu>
2019-10-04 18:14:43 -0400

    check in tests

---
+++ src/tests-6.1-req.scm
@@ -0,0 +1,10 @@
+(add-tests-with-string-output "define function"
+  [(let ()
+     (define (x) 3)
+     (define (y) 4)
+     (fx+ (x) (y)))
+   => "7\n"]
+  [(let ()
+     (define (f x y) (fx+ x y))
+     (f 3 4))
+   => "7\n"])
